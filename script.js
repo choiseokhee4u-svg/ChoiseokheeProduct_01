d3830c5d981b5d047aac08ad0d144d8f19355fe7
choiseokhee4u-svg <choiseokhee4u@gmail.com>
2026-02-17 23:33:53 +0900

    Update Water icon to Water_New.jpg and remove filter

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ const E = {
     FIRE: { img: 'images/Fire.png', c: '#f87171', style: 'filter: invert(1) hue-rotate(180deg);' },
     EARTH: { img: 'images/Earth.png', c: '#fbbf24', style: 'filter: invert(1) hue-rotate(180deg);' },
     METAL: { img: 'images/Metal.png', c: '#e2e8f0', style: 'filter: invert(1) hue-rotate(180deg);' },
-    WATER: { img: 'images/Water.jpg', c: '#60a5fa', style: 'filter: invert(1) hue-rotate(180deg);' }
+    WATER: { img: 'images/Water_New.jpg', c: '#60a5fa' }
 };
 const EM = { '甲': 'WOOD', '乙': 'WOOD', '丙': 'FIRE', '丁': 'FIRE', '戊': 'EARTH', '己': 'EARTH', '庚': 'METAL', '辛': 'METAL', '壬': 'WATER', '癸': 'WATER', '寅': 'WOOD', '卯': 'WOOD', '巳': 'FIRE', '午': 'FIRE', '辰': 'EARTH', '戌': 'EARTH', '丑': 'EARTH', '未': 'EARTH', '申': 'METAL', '酉': 'METAL', '亥': 'WATER', '子': 'WATER' };
 const STEM_EL = { '甲': 'WOOD', '乙': 'WOOD', '丙': 'FIRE', '丁': 'FIRE', '戊': 'EARTH', '己': 'EARTH', '庚': 'METAL', '辛': 'METAL', '壬': 'WATER', '癸': 'WATER' };
